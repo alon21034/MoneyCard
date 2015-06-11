2684fd44906b861f800183cba3d6101f574f12df
alon21034 <lee790909@gmail.com>
2015-06-11 20:27:09 +0800

    init

---
+++ app/src/main/java/tw/ntu/edu/alon21034/moneycard/MainActivity.java
@@ -0,0 +1,109 @@
+package tw.ntu.edu.alon21034.moneycard;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private TextView mTextView;
+    public final static String FILTER = "tw.ntu.edu.alon21034.MainReciever";
+    public final static String KEY_BROADCAST_MODE = "MODE";
+    private final static String PREFS_NAME = "MONEY";
+    public final static String KEY_MONEY = "KEY_MONEY";
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getIntExtra(KEY_BROADCAST_MODE, 0)) {
+                case 0:
+                    showMoney(money_num);
+                    break;
+                case 1:
+                    money_num += 100;
+                    showMoney(money_num);
+                    break;
+                case 2:
+                    money_num -= 50;
+                    showMoney(money_num);
+                    break;
+                default:
+                    showMoney(money_num);
+                    break;
+            }
+        }
+    };
+
+    public static int money_num;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.main_value_textview);
+
+        registerReceiver(receiver, new IntentFilter(FILTER));
+
+        money_num = loadFromPrefs(KEY_MONEY);
+
+        showMoney(money_num);
+
+        Log.d("!!", "!!! on create");
+    }
+
+    public void showMoney(int n) {
+        mTextView.setText(n + "$");
+    }
+
+    public void updateMoney(int n) {
+        showMoney(n);
+        saveToPrefs(n, KEY_MONEY);
+    }
+
+    private int loadFromPrefs(String key) {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        int n = prefs.getInt(key, 1000);
+        return n;
+    }
+
+    private void saveToPrefs(int n, String key) {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(key, n);
+        editor.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/tw/ntu/edu/alon21034/moneycard/MyHostApduService.java
@@ -0,0 +1,64 @@
+package tw.ntu.edu.alon21034.moneycard;
+
+import android.content.Intent;
+import android.nfc.cardemulation.HostApduService;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by alon21034 on 2015/6/11.
+ */
+public class MyHostApduService extends HostApduService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("!!", "!! on create");
+    }
+
+    private byte[] getWelcomeMessage() {
+        return "Hello".getBytes();
+    }
+
+    private byte[] getNextMessage() {
+        return new byte[]{(byte)0x01, (byte)0x23, (byte)0x34, (byte)0x56};
+    }
+
+    @Override
+    public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
+        if (selectAidApdu(commandApdu)) {
+            Log.d("!!", "!! Application selected");
+            return getWelcomeMessage();
+        } else {
+            Intent intent = new Intent();
+            intent.setAction(MainActivity.FILTER);
+            if (commandApdu[0] == (byte)0xaa) {
+                intent.putExtra(MainActivity.KEY_BROADCAST_MODE, 0);
+                sendBroadcast(intent);
+                Log.d("!!","!! query");
+            } else if (commandApdu[0] == (byte)0xbb) {
+                intent.putExtra(MainActivity.KEY_BROADCAST_MODE, 1);
+                sendBroadcast(intent);
+                Log.d("!!","!! add");
+            } else if (commandApdu[0] == (byte)0xcc) {
+                intent.putExtra(MainActivity.KEY_BROADCAST_MODE, 2);
+                sendBroadcast(intent);
+                Log.d("!!", "!! sub");
+            } else {
+
+            }
+            sendResponseApdu(new byte[]{(byte) MainActivity.money_num});
+            return new byte[]{(byte)MainActivity.money_num};
+        }
+    }
+
+    @Override
+    public void onDeactivated(int reason) {
+        Log.d("!!", "!! on deactivated " + reason);
+
+    }
+
+    private boolean selectAidApdu(byte[] apdu) {
+        return apdu.length >= 2 && apdu[0] == (byte)0 && apdu[1] == (byte)0xa4;
+    }
+}
